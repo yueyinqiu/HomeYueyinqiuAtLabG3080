7f0267f515b52090b934b267c8b5c6380144f6ca
yueyinqiu <yueyinqiu@outlook.com>
2026-08-09 15:06:14 +0000

    s

--- switch.sh
+++ switch.sh
@@ -1,12 +1,1 @@
-read -p "target: " target
-
-echo "BUILD"
-home-manager build --flake .#yueyinqiu@lab-g3080
-
-echo "COPY"
-nix copy --to "ssh://$target" ./result
-
-echo "ACTIVATE"
-ssh "$target" -- HOME_MANAGER_BACKUP_EXT="backup" "$(readlink -f ./result/activate)"
-
-echo "ssh"" ""$target"
+home-manager switch --flake .
